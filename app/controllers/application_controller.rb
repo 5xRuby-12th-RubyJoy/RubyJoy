b1520058a2e16d379a60658616a245b5e86ccdd8
Elijah Chen <elijahbecerrakc@gmail.com>
2022-12-04 19:29:00 +0800

    log in & registration done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-private
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
-  end
-
-  def record_not_found
-    render file: "#{Rails.root}/public/404.html",
-           status: 404,
-           layout: false
   end
 end
