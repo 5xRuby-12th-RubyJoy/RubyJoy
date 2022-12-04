6b32538fec17bc46e3fe104cbb0962d553e21bb1
Elijah Chen <elijahbecerrakc@gmail.com>
2022-12-04 19:29:00 +0800

    log in & registration done

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
   devise_scope :user do
     get "sign_in", to: "devise/sessions#new"
-    get 'new_vender_registration', to: "users/registrations#vender_new"
-    post 'vender_sign_up', to: "users/registrations#vender_create"
-
   end
 
   resources :orders, only: [:create] do
