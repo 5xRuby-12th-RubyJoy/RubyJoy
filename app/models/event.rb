3811e10d05851c23329d3ec0f35e1b04880b5a05
wayne035 <119384952+wayne035@users.noreply.github.com>
2022-12-06 15:26:08 +0800

    Event model crud (#11)
    * add events controller
    
    * eventsCRUD
    
    * 更新edit
    
    label 連結
    
    * 更新 new 頁面
    
    修改label 連結
    
    * 修改controller  create & update 判斷式
    
    修改controller  create & update 判斷式
    
    Co-authored-by: HsiehChengTing <112834036+Danny3766@users.noreply.github.com>

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,13 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+
+  validates :title, presence: true
+  validates :subtitle, presence: true
+  validates :description, presence: true
+  validates :venue, presence: true
+  
+
   has_many :products,through :event_product
+
 end
