4f559293b876c58ca0e657042869a5674be264ee
吳家瑋 <106542350+wucheiwei@users.noreply.github.com>
2022-12-06 12:30:39 +0800

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home; end
 
-  def sign_in_session; end
+  def sign_in_session 
+  end
 
-  def sign_up_session; end
+  def sign_up_session 
+  end
 end
