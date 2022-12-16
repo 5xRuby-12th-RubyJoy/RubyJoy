f1524eb72ed1c964a403211e4df98e929cf88ccf
HsiehChengTing <112834036+Danny3766@users.noreply.github.com>
2022-12-16 16:16:48 +0800

    修改，讓404頁面正常顯示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
 
