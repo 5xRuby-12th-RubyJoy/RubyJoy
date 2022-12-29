00d6dab981db3d21831cc9596b67f72c7e80e5d9
HsiehChengTing <112834036+Danny3766@users.noreply.github.com>
2022-12-29 14:56:24 +0800

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = '<5xruby12thrubyjoy@5xruby12thrubyjoy.com>'
+  config.mailer_sender = '<5xruby12thrubyjoy@rubyjoy.com>'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
