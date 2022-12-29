8cafd02801f5c32634aa721284cf18722577adb2
HsiehChengTing <112834036+Danny3766@users.noreply.github.com>
2022-12-29 14:51:24 +0800

    Update stores_controller.rb

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -70,7 +70,6 @@ class StoresController < ApplicationController
   def find_store
     @store = Store.find_by(id: params[:store_id])
     return unless current_user_store?
-
     @store = current_user.store
   end
 
