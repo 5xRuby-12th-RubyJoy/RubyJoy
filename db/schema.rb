49bf2dc2e1f6600db0559ebd26e5491ad995f845
zack <twd1000015706@gmail.com>
2022-12-30 11:32:51 +0800

    argument_name_change

--- db/schema.rb
+++ db/schema.rb
@@ -143,12 +143,12 @@ ActiveRecord::Schema.define(version: 2022_12_29_080248) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
